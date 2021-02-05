e21ba4be4ee6d8a01c427e6c6648c2c34c4aab0a
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-02-04 22:48:01 -0300

    fixed bug to display value

--- scripts.js
+++ scripts.js
@@ -103,9 +103,9 @@ const DOM = {
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value) * 100
+    value = value * 100
 
-    return value
+    return Math.round(value)
   },
   formatCurrency(value) {
     const signal = Number(value) < 0 ? '-' : ''
